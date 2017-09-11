272ac94361c55227963f20a5718ff481a4195cfd
Thenanox <thenanox@gmail.com>
2017-09-12 00:29:08 +0200

    feat(websocket): websocket first try

--- dist/index.js
+++ dist/index.js
@@ -2,22 +2,33 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const child_process_1 = require("child_process");
 const process_1 = require("process");
+const websocket = require("ws");
 const SC2PATH = process_1.env['SC2PATH'];
 class SC2Node {
     constructor(path, args) {
-        console.log(path, args);
-        console.log(SC2PATH);
         const sc2exe = SC2PATH || "C:/Program Files (x86)/Starcraft II/";
         this.sc2 = child_process_1.spawn("../Versions/Base56787/SC2_x64.exe", ["-listen", "127.0.0.1", "-port", "8190", "-displayMode", "0"], { cwd: sc2exe + "/Support64" });
-        this.sc2.on('data', (data) => {
-            console.log(data);
-        });
-        this.sc2.on('error', (error) => {
-            console.error('Stacraft 2 error, ', error);
-        });
-        this.sc2.on('close', (code) => {
-            console.log('Starcraft 2 closed with code, ' + code);
-        });
+        this.sc2.on('error', (error) => console.error('SC2 error, ', error));
+        this.sc2.on('close', (code) => console.log('SC2 closed with code, ' + code));
+        this.sc2.on('exit', (exit) => console.log('SC2 exited because,', exit));
+        let connected = false;
+        while (!connected) {
+            console.log('wait');
+            this.ws = new websocket('ws://127.0.0.1:8190/sc2api');
+            this.ws.on('open', () => {
+                console.log('Websocket opened to SC2');
+                connected = true;
+            });
+            this.ws.on('message', (data) => console.log("wsdata", data));
+            this.ws.on("error", (error) => {
+                console.error("wserror", error);
+                setTimeout(() => connected = false, 1000);
+            });
+        }
+        console.log('done');
+    }
+    createGame() {
+        this.ws.send('data', (error) => console.log(error));
     }
 }
 exports.SC2Node = SC2Node;
--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,29 @@
 import { spawn, ChildProcess } from "child_process";
 import { env } from "process";
+import * as websocket from "ws";
 
 const SC2PATH = env['SC2PATH']; 
 
 export class SC2Node {
 
     private sc2: ChildProcess;
+    private ws: websocket;
     
     constructor(path?: string, args?: string[]){
-        console.log(path, args);
-        console.log(SC2PATH);
         const sc2exe = SC2PATH || "C:/Program Files (x86)/Starcraft II/" 
         this.sc2 = spawn("../Versions/Base56787/SC2_x64.exe", ["-listen", "127.0.0.1", "-port", "8190", "-displayMode", "0"], {cwd: sc2exe + "/Support64"});
-        this.sc2.on('data', (data) => {
-            console.log(data);
-        })
         
-        this.sc2.on('error', (error) => {
-            console.error('Stacraft 2 error, ', error);
-        })
-        
-        this.sc2.on('close', (code) => {
-            console.log('Starcraft 2 closed with code, ' + code);
-        });
+        this.sc2.on('error', (error) => console.error('SC2 error, ', error));
+        this.sc2.on('close', (code) => console.log('SC2 closed with code, ' + code));
+        this.sc2.on('exit', (exit) => console.log('SC2 exited because,', exit));
+    }
+
+    connectWebsocket() {
+        this.ws = new websocket('ws://127.0.0.1:8190/sc2api');
+        this.ws.on('open', () => console.log('Websocket opened to SC2'));
+        this.ws.on('message', (data) => console.log("wsdata", data));
+        this.ws.on("error", (error) => console.error("wserror", error));
+        this.ws.send('data', (error) => console.log(error));
     }
    
 }
