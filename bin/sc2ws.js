687249fb244f43145119183265ad54da71f57bc1
Thenanox <thenanox@gmail.com>
2017-09-12 22:46:56 +0200

    feat(create): first version for create and join game

--- bin/sc2ws.js
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env node
-
-const SC2Node = require('../dist/index.js').SC2Node;
-const result = new SC2Node();
-result.connectWebsocket();
-result.createGame();
--- dist/index.js
+++ dist/index.js
@@ -3,25 +3,70 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const child_process_1 = require("child_process");
 const process_1 = require("process");
 const websocket = require("ws");
-const requestCreate = require('./messages/requestcreategame');
-const SC2PATH = process_1.env['SC2PATH'];
+const sc2api = require("./messages/s2clientprotocol/sc2api_pb");
+const SC2PATH = process_1.env["SC2PATH"];
 class SC2Node {
     launchSC2() {
         const sc2exe = SC2PATH || "C:/Program Files (x86)/Starcraft II/";
         this.sc2 = child_process_1.spawn("../Versions/Base56787/SC2_x64.exe", ["-listen", "127.0.0.1", "-port", "8190", "-displayMode", "0"], { cwd: sc2exe + "/Support64" });
-        this.sc2.on('error', (error) => console.error('SC2 error, ', error));
-        this.sc2.on('close', (code) => console.log('SC2 closed with code, ' + code));
-        this.sc2.on('exit', (exit) => console.log('SC2 exited because,', exit));
+        this.sc2.on("error", (error) => console.error("SC2 error, ", error));
+        this.sc2.on("close", (code) => console.log("SC2 closed with code, " + code));
+        this.sc2.on("exit", (exit) => console.log("SC2 exited because, ", exit));
     }
     connectWebsocket() {
-        this.ws = new websocket('ws://127.0.0.1:8190/sc2api');
-        this.ws.on('open', () => console.log('Websocket opened to SC2'));
-        this.ws.on('message', (data) => console.log("wsdata", data));
+        this.ws = new websocket("ws://127.0.0.1:8190/sc2api");
+        this.ws.on("open", () => console.log("Websocket opened to SC2"));
+        this.ws.on("message", (data) => {
+            const bytes = Array.prototype.slice.call(data, 0);
+            const resp = sc2api.Response.deserializeBinary(bytes);
+            console.log("wsdata", resp.toObject());
+        });
         this.ws.on("error", (error) => console.error("wserror", error));
     }
+    requestPing() {
+        const request = new sc2api.Request();
+        request.setPing(new sc2api.RequestPing());
+        this.ws.send(request.serializeBinary());
+    }
     createGame() {
-        const req = new requestCreate.requestcreategame();
-        this.ws.send(req);
+        const request = new sc2api.Request();
+        const req = new sc2api.RequestCreateGame();
+        const p1 = new sc2api.PlayerSetup();
+        p1.setType(sc2api.PlayerType.PARTICIPANT);
+        const p2 = new sc2api.PlayerSetup();
+        p2.setType(sc2api.PlayerType.COMPUTER);
+        p2.setRace(sc2api.Race.ZERG);
+        p2.setDifficulty(sc2api.Difficulty.VERYEASY);
+        req.addPlayerSetup(p1);
+        req.addPlayerSetup(p2);
+        const localMap = new sc2api.LocalMap();
+        localMap.setMapPath("Melee/Simple64.SC2Map");
+        req.setLocalMap(localMap);
+        req.setRealtime(true);
+        request.setCreateGame(req);
+        this.ws.send(request.serializeBinary());
+    }
+    joinGame() {
+        const request = new sc2api.Request();
+        const req = new sc2api.RequestJoinGame();
+        req.setRace(sc2api.Race.TERRAN);
+        const interfaceOptions = new sc2api.InterfaceOptions();
+        interfaceOptions.setScore(true);
+        const camera = new sc2api.SpatialCameraSetup();
+        camera.setWidth(24);
+        const resolution = new sc2api.Size2DI();
+        resolution.setX(84);
+        resolution.setY(84);
+        camera.setResolution(resolution);
+        const minimap = new sc2api.Size2DI();
+        minimap.setX(64);
+        minimap.setY(64);
+        camera.setMinimapResolution(minimap);
+        interfaceOptions.setFeatureLayer(camera);
+        req.setOptions(interfaceOptions);
+        request.setJoinGame(req);
+        console.log(request.toObject());
+        this.ws.send(request.serializeBinary());
     }
 }
 exports.SC2Node = SC2Node;
